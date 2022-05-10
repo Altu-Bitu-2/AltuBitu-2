297589551856492d3f3d6063c96ad1469b8d3e71
gkim <kimkm0419@naver.com>
2022-05-11 00:26:57 +0900

    0503 Sample commented ver : Update essentials

--- #14 0503_최단경로/Sample_commented/BOJ_11559_sample.cpp
+++ #14 0503_최단경로/Sample_commented/BOJ_11559_sample.cpp
@@ -11,67 +11,68 @@ const int ROW = 6;
 const int COL = 12;
 
 bool bfs(int r, int c, vector<vector<char>> &board) {
+    //상, 하, 좌, 우
     int dr[4] = {-1, 1, 0, 0};
     int dc[4] = {0, 0, -1, 1};
 
     queue<ci> q;
     queue<ci> puyo; //포함된 좌표 저장할 큐
-    vector<vector<bool>> visited(ROW, vector<bool>(COL, false));
+    vector<vector<bool>> visited(ROW, vector<bool>(COL, false));    // 확인한 좌표..
 
     //시작 초기화
     q.push({r, c});
     puyo.push({r, c});
     visited[r][c] = true;
     int cnt = 1; //모여있는 뿌요의 개수
-    while (!q.empty()) {
-        int cr = q.front().first;
-        int cc = q.front().second;
-        q.pop();
+    while (!q.empty()) {    // 큐의 크기만큼 반복
+        int cr = q.front().first;   // cr 업데이트
+        int cc = q.front().second;  // cc 업데이트
+        q.pop();    // q 원소 pop
 
-        for (int i = 0; i < 4; i++) {
-            int nr = cr + dr[i];
-            int nc = cc + dc[i];
+        for (int i = 0; i < 4; i++) {    // 4번 반복 (4방향)
+            int nr = cr + dr[i];        // nr (row)
+            int nc = cc + dc[i];        // nc (column)
             if (nr < 0 || nr >= ROW || nc < 0 || nc >= COL || visited[nr][nc]) {
-                continue;
+                continue;   // 계속
             }
-            if (board[nr][nc] == board[cr][cc]) {
-                q.push({nr, nc});
-                puyo.push({nr, nc});
-                visited[nr][nc] = true;
-                cnt++;
+            if (board[nr][nc] == board[cr][cc]) {   // 필드를 비교했을 떄 같으면
+                q.push({nr, nc});   // q 업데이트
+                puyo.push({nr, nc});    // 좌표 저장
+                visited[nr][nc] = true; // 확인한 위치로 표시
+                cnt++;  // 모여있는 뿌요 개수 증가
             }
         }
     }
     if (cnt < 4) { //뿌요 안터짐
-        return false;
+        return false;   // 거짓 반환
     }
-    while (!puyo.empty()) {
-        int cr = puyo.front().first;
-        int cc = puyo.front().second;
-        puyo.pop();
+    while (!puyo.empty()) { // 저장된 좌표가 있으면
+        int cr = puyo.front().first;    // cr 업데이트
+        int cc = puyo.front().second;   // cc 업데이트
+        puyo.pop(); // 업데이트한 내용 pop
 
-        board[cr][cc] = '.';
+        board[cr][cc] = '.';    // 필드를 빈공간으로...
     }
-    return true;
+    return true;    // 참 반환
 }
 
 // 뿌요를 터뜨린 이후의 새 필드를 작성하는 함수
 vector<vector<char>> makeNewBoard(vector<vector<char>> &board) {
-    vector<vector<char>> new_board(ROW, vector<char>(COL, '.'));
-    for (int i = 0; i < ROW; i++) {
+    vector<vector<char>> new_board(ROW, vector<char>(COL, '.'));    // 새 필드...
+    for (int i = 0; i < ROW; i++) { // 12줄만큼 반복
         queue<char> temp; //남아있는 뿌요 모으는 큐
-        for (int j = 0; j < COL; j++) {
-            if (board[i][j] != '.') {
-                temp.push(board[i][j]);
+        for (int j = 0; j < COL; j++) { // 6개의 문자 수만큼 반복
+            if (board[i][j] != '.') {   // 문자의 위치에 .(빈공간)이 입력되지 않았다면...
+                temp.push(board[i][j]); // 남아있는 뿌오 모으는 큐에 해당 위치의 색상 저장
             }
         }
-        int index = 0;
-        while (!temp.empty()) {
-            new_board[i][index++] = temp.front();
-            temp.pop();
+        int index = 0;  // 인덱스 선언&초기화
+        while (!temp.empty()) { // 남아있는 뿌요가 존재한다면
+            new_board[i][index++] = temp.front();   // 새 필드에 남아있는 뿌요 저장
+            temp.pop(); // 저장된 남아있는 뿌요 비우기
         }
     }
-    return new_board;
+    return new_board;   // 새 필드 반환
 }
 
 int simulation(vector<vector<char>> &board) {
--- #14 0503_최단경로/Sample_commented/BOJ_18111_sample.cpp
+++ #14 0503_최단경로/Sample_commented/BOJ_18111_sample.cpp
@@ -7,48 +7,45 @@ using namespace std;
 const int INF = 1e9;
 
 int mineLand(int n, int m, int b, int height, vector<vector<int>> &land) {
-    int tot_time = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (land[i][j] > height) { //블록 제거
-                b += (land[i][j] - height);
-                tot_time += 2 * (land[i][j] - height);
-            } else if (land[i][j] < height) { //블록 쌓기
-                b -= (height - land[i][j]);
-                tot_time += (height - land[i][j]);
+    int tot_time = 0;   // 총시간 초기화
+    for (int i = 0; i < n; i++) {   // 세로 크기만큼 반복
+        for (int j = 0; j < m; j++) {   // 가로 크기만큼 반복
+            if (land[i][j] > height) { // 블록 제거 : 땅의 높이가 확인하는 높이보다 높을 때
+                b += (land[i][j] - height); // 블록개수 업데이트
+                tot_time += 2 * (land[i][j] - height);  // 총 시간 업데이트
+            } else if (land[i][j] < height) { // 블록 쌓기 : 땅의 높이가 확인하는 높이보다 낮을 때
+                b -= (height - land[i][j]); // 블록개수 업데이트
+                tot_time += (height - land[i][j]);  // 총 시간 업데이트
             }
         }
     }
     if (b < 0) //최종적으로 블럭이 음수면 불가능한 높이
         return INF + 1;
-    return tot_time;
+    return tot_time;    // 총 시간 반환
 }
 
 int main() {
-    int n, m, b, min_height = 256, max_height = 0;
+    int n, m, b, min_height = 256, max_height = 0;  // 세로크기, 가로크기, 블록개수...
 
-    //입력
-    cin >> n >> m >> b;
-    vector<vector<int>> land(n, vector<int>(m, 0));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> land[i][j];
+    cin >> n >> m >> b;     // 세로&가로 크기, 블록 개수 입력받기
+    vector<vector<int>> land(n, vector<int>(m, 0)); // 땅의 높이 입력받을 위치
+    for (int i = 0; i < n; i++) {   // 세로 크기만큼 반복
+        for (int j = 0; j < m; j++) {   // 가로 크기만큼 반복
+            cin >> land[i][j];  // 땅의 높이 입력받기
             min_height = min(min_height, land[i][j]); //제일 낮은 땅 높이
             max_height = max(max_height, land[i][j]); //제일 높은 땅 높이
         }
     }
 
-    //연산
-    int min_time = INF, height = 0;
-    for (int i = min_height; i <= max_height; i++) {
-        int t = mineLand(n, m, b, i, land);
-        if (t <= min_time) { //가장 빨리 작업이 끝나는 높이
-            min_time = t;
-            height = i;
+    int min_time = INF, height = 0;     // 최단 시간, 높이
+    for (int i = min_height; i <= max_height; i++) {    // 제일 낮은 땅부터 제일 높은 땅 높이까지..
+        int t = mineLand(n, m, b, i, land);     // 연산
+        if (t <= min_time) { // 가장 빨리 작업이 끝나는 높이
+            min_time = t;   // 땅을 고르는 데 걸리는 최단 시간 저장
+            height = i;     // 땅의 높이 저장
         }
     }
 
-    //출력
-    cout << min_time << ' ' << height;
+    cout << min_time << ' ' << height;  // 연산 결과 출력
     return 0;
 }
