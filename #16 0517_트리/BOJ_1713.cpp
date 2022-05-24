ffbc5f9387676ea88f0028845e1072a42e60ce61
gkim <kimkm0419@naver.com>
2022-05-25 00:25:22 +0900

    0517 Sample commented : Update essentials

--- #16 0517_트리/BOJ_1713.cpp
+++ #16 0517_트리/BOJ_1713.cpp
@@ -8,35 +8,32 @@
 using namespace std;
 typedef pair<int, int> ci;
 
-bool cmp(const pair<int, ci> &c1, const pair<int, ci> &c2) {
-    if (c1.second.first != c2.second.first) {
-        return c1.second.first < c2.second.first;
+bool cmp(const pair<int, ci> &c1, const pair<int, ci> &c2) {    // 오름차순 정렬
+    if (c1.second.first != c2.second.first) {   // 둘이 같지 않다면
+        return c1.second.first < c2.second.first;   // 크기 비교 결과 반환
     }
-    return c1.second.second < c2.second.second;
+    return c1.second.second < c2.second.second; // 크기 비교 결과 반환
 }
 
 int main() {
-    int n, m, input;
+    int n, m, input;    // 사진 개수, 추천 횟수, 학생 번호
 
-    //입력 & 연산
-    cin >> n >> m;
-    map<int, ci> candidate; //first: 후보 학생, second: {추천 횟수, 게시 시간}
-    for (int i = 0; i < m; i++) {
-        cin >> input;
-        //비어있는 사진틀이 없는 경우
-        if (candidate.size() == n && candidate.find(input) == candidate.end()) {
-            candidate.erase(min_element(candidate.begin(), candidate.end(), cmp));
+    cin >> n >> m;  // 사진 개수, 추천 횟수, 학생 번호 반환
+    map<int, ci> candidate; // first: 후보 학생, second: {추천 횟수, 게시 시간}
+    for (int i = 0; i < m; i++) {   // 추천 횟수만큼 반복
+        cin >> input;   // 추천한 학생 번호 입력받기
+
+        if (candidate.size() == n && candidate.find(input) == candidate.end()) {     // 비어있는 사진틀이 없는 경우
+            candidate.erase(min_element(candidate.begin(), candidate.end(), cmp));  // 추천 횟수가 가장 적은 학생의 사진 삭제
         }
-        //첫 게시라면
-        if (candidate.find(input) == candidate.end()) {
-            candidate[input].second = i;
+        if (candidate.find(input) == candidate.end()) { //첫 게시라면
+            candidate[input].second = i;    // 추천 횟수 업데이트
         }
-        candidate[input].first++; //추천 횟수 증가
+        candidate[input].first++; // 추천 횟수 증가
     }
 
-    //출력
-    for (auto iter = candidate.begin(); iter != candidate.end(); iter++) {
-        cout << iter->first << ' ';
+    for (auto iter = candidate.begin(); iter != candidate.end(); iter++) {  // 증가하는 순서대로...
+        cout << iter->first << ' '; // 최종 후보의 학생 번호 출력
     }
     return 0;
 } 
